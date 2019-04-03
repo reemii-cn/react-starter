acd3830130139a9ede50571c752502115c3022e6
龚江鹏 <494919586@qq.com>
2019-04-03 16:26:58 +0800

    delete unuse

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,8 +2,6 @@ import React, { Component } from 'react'
 import logo from '../assets/logo.svg'
 import './App.css'
 import { Button } from 'antd'
-import Router from 'src/router/router'
-import { Storakie } from 'src/core/storage'
 
 class App extends Component {
   render() {
@@ -12,16 +10,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>{process.env.REACT_APP_NAME}</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
           <Button>ReeMii</Button>
-          <Router />
         </header>
       </div>
     )
